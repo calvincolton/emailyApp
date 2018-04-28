170bf8d0fb349ce8ffedab2218313733357b72e9
Calvin Colton <calvincolton@gmail.com>
2018-04-28 15:27:56 -0700

    passport touchups, nearly complete

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
     }
   )
 );
@@ -28,6 +30,8 @@ app.get(
     scope: ['profile', 'email']
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
