3928a9ecabcf483eca8c2a551ab752a1eb557b59
Calvin Colton <calvincolton@gmail.com>
2018-05-03 09:28:51 -0700

    <a> tag to <Link>, adding a ternary expression for logo redirect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,15 @@
+// modules
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
-
+// components
 import Header from './Header';
+import Landing from './Landing';
+// actions
+import * as actions from '../actions';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
