afe668f8657dfb84766fa24db5374e7853c507cb
Calvin Colton <calvincolton@gmail.com>
2018-05-05 12:37:20 -0700

    handleToken action creator, adding as callback in StripeCheckout

--- client/src/components/StripeWrapper.js
+++ client/src/components/StripeWrapper.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
+import * as actions from '../actions';
 
 class StripeWrapper extends Component {
   render() {
@@ -9,7 +11,7 @@ class StripeWrapper extends Component {
         name="Emaily"
         description="$5 for 5 credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add Credits</button>
@@ -18,4 +20,4 @@ class StripeWrapper extends Component {
   }
 }
 
-export default StripeWrapper;
+export default connect(null, actions)(StripeWrapper);
