9dfe4ce153cab9cef72e6590bde69e84d20b7ca7
Calvin Colton <calvincolton@gmail.com>
2018-05-07 21:22:31 -0700

    express routing for client-side assets in production

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 const billingRoutes = require('./routes/billingRoutes');
 require('./models/User');
@@ -27,6 +27,20 @@ app.use(passport.session());
 // routes
 authRoutes(app);
 billingRoutes(app);
+
+// handling client assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
