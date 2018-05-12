705cb1a082792a548356517f7deb7317be29e319
Calvin Colton <calvincolton@gmail.com>
2018-05-11 21:27:46 -0700

    adding link to thank you page from email survey, using keys

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,5 @@
+const keys = require('../../config/keys');
+
 module.exports = (survey) => {
   const { body } = survey;
   return (`
@@ -7,10 +9,10 @@ module.exports = (survey) => {
           <h3>We want your feedback</h3>
           <p>${survey.body}</p>
           <div>
-            <a href="http://localhost:3000">Yes</a>
+            <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
           </div>
           <div>
-            <a href="http://localhost:3000">No</a>
+            <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
           </div>
         </div>
       </body>
