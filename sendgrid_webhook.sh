2be9e19f2fd57b8eaab4ad91bb05cdbb40787e6e
Calvin Colton <calvincolton@gmail.com>
2018-05-17 16:28:20 -0700

    sendgrid webhook subdomain flag edit

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,5 +1,5 @@
 function localtunnel {
-  lt --subdomain myuniquesubdomain1479haight --port 5000
+  lt -s haight2001ox00ashbury --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
